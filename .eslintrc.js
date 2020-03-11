d41e1307be95894996767159460b12a70e6ff434
William Reiske <wreiske@mieweb.com>
2020-03-11 11:58:51 -0400

    Updated README markdown formatting (#8)
    * Updated README markdown formatting
    * Added EditorConfig, VSCode configs, and updated package.json
    * Moved period in readme, removed extra end of line char
    * LGTM Badges

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,6 @@
 module.exports = {
   parser: '@typescript-eslint/parser',
-  extends: [
-    'plugin:@typescript-eslint/recommended',
-    'prettier/@typescript-eslint',
-    'plugin:prettier/recommended'
-  ],
+  extends: ['plugin:@typescript-eslint/recommended', 'prettier/@typescript-eslint', 'plugin:prettier/recommended'],
   parserOptions: {
     ecmaVersion: 2018,
     sourceType: 'module'
@@ -12,17 +8,9 @@ module.exports = {
   rules: {
     'block-scoped-var': 'error',
     'comma-dangle': ['error', 'never'],
-    'lines-between-class-members': [
-      'error',
-      'always',
-      { exceptAfterSingleLine: true }
-    ],
+    'lines-between-class-members': ['error', 'always', { exceptAfterSingleLine: true }],
     'max-depth': 'error',
-    'max-len': [
-      'error',
-      { code: 120 },
-      { tabWidth: 2 }
-    ],
+    'max-len': ['error', { code: 120 }, { tabWidth: 2 }],
     'no-bitwise': ['error', { allow: ['~'] }],
     'no-console': 'error', // Use debug instead
     'no-underscore-dangle': 'error',
@@ -38,11 +26,7 @@ module.exports = {
     'prefer-arrow-callback': 'warn',
     'prefer-const': 'error',
     'prefer-promise-reject-errors': ['error', { allowEmptyReject: true }],
-    'quotes': [
-      'error',
-      'single',
-      { avoidEscape: true }
-    ],
+    quotes: ['error', 'single', { avoidEscape: true }],
     semi: ['error', 'always'],
     '@typescript-eslint/no-inferrable-types': 0
   }
