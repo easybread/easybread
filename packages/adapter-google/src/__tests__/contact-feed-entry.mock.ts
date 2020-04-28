637e4b9427169dd8fc77e805a4ebc157a158f95d
Aleksander Cherednichenko <alexandr2110pro@gmail.com>
2020-04-28 15:41:35 +0300

    adapter-google: people update operation handler

---
+++ packages/adapter-google/src/__tests__/contact-feed-entry.mock.ts
@@ -0,0 +1,77 @@
+export const CONTACT_FEED_ENTRY_CREATE_MOCK = {
+  version: '1.0',
+  encoding: 'UTF-8',
+  entry: {
+    id: {
+      $t:
+        'http://www.google.com/m8/feeds/contacts/testuser%40mail.com/base/79ec2071883179b9'
+    },
+    gd$etag: '"R3k4eTVSLyt7I2A9XB5UE0wKTgU."',
+    updated: {
+      $t: '2020-04-19T15:41:56.731Z'
+    },
+    app$edited: {
+      xmlns$app: 'http://www.w3.org/2007/app',
+      $t: '2020-04-19T15:41:56.731Z'
+    },
+    category: [
+      {
+        scheme: 'http://schemas.google.com/g/2005#kind',
+        term: 'http://schemas.google.com/contact/2008#contact'
+      }
+    ],
+    title: {
+      $t: 'Test Contact'
+    },
+    link: [
+      {
+        rel: 'http://schemas.google.com/contacts/2008/rel#photo',
+        type: 'image/*',
+        href:
+          'https://www.google.com/m8/feeds/photos/media/testuser%40mail.com/79ec2071883179b9'
+      },
+      {
+        rel: 'self',
+        type: 'application/atom+xml',
+        href:
+          'https://www.google.com/m8/feeds/contacts/testuser%40mail.com/full/79ec2071883179b9'
+      },
+      {
+        rel: 'edit',
+        type: 'application/atom+xml',
+        href:
+          'https://www.google.com/m8/feeds/contacts/testuser%40mail.com/full/79ec2071883179b9'
+      }
+    ],
+    gd$name: {
+      gd$fullName: {
+        $t: 'Test Contact'
+      },
+      gd$givenName: {
+        $t: 'Test'
+      },
+      gd$familyName: {
+        $t: 'Contact'
+      }
+    },
+    gd$email: [
+      {
+        address: 'test@mail.com',
+        primary: 'true',
+        rel: 'http://schemas.google.com/g/2005#work'
+      }
+    ],
+    gd$phoneNumber: [
+      {
+        rel: 'http://schemas.google.com/g/2005#home',
+        primary: 'true',
+        uri: 'tel:+7-965-444-22-11',
+        $t: '+7 (965) 444 2211'
+      }
+    ],
+    xmlns: 'http://www.w3.org/2005/Atom',
+    xmlns$batch: 'http://schemas.google.com/gdata/batch',
+    xmlns$gContact: 'http://schemas.google.com/contact/2008',
+    xmlns$gd: 'http://schemas.google.com/g/2005'
+  }
+};
---
+++ packages/adapter-google/src/transform/google.update-contact.transform.ts
@@ -0,0 +1,86 @@
+import { cloneDeep, merge } from 'lodash';
+
+import { GoogleContactsFeedEntry } from '../interfaces';
+
+export const googleUpdateContactTransform = (
+  baseEntry: GoogleContactsFeedEntry,
+  updateEntry: GoogleContactsFeedEntry
+): GoogleContactsFeedEntry => {
+  const baseEntryClone = cloneDeep(baseEntry);
+
+  const {
+    gd$email,
+    gd$phoneNumber,
+    gd$name,
+    title,
+    // TODO: handle commented out fields
+    // gContact$groupMembershipInfo,
+    // gContact$website,
+    // gd$extendedProperty,
+    gd$organization
+    // link
+  } = updateEntry;
+
+  if (title) {
+    baseEntryClone.title = { $t: title.$t };
+  }
+
+  if (gd$email && gd$email.length) {
+    updateArrayField(
+      baseEntryClone.gd$email,
+      gd$email,
+      (base, update) => base.rel === update.rel,
+      (base, update) => {
+        base.address = update.address;
+      }
+    );
+  }
+
+  if (gd$phoneNumber && gd$phoneNumber.length) {
+    updateArrayField(
+      baseEntryClone.gd$phoneNumber,
+      gd$phoneNumber,
+      (base, update) => base.rel === update.rel,
+      (base, update) => {
+        base.$t = update.$t;
+        base.uri = update.uri;
+      }
+    );
+  }
+
+  if (gd$name) {
+    merge(baseEntryClone.gd$name, gd$name);
+  }
+
+  if (gd$organization) {
+    updateArrayField(
+      baseEntryClone.gd$organization,
+      gd$organization,
+      (base, update) => base.rel === update.rel,
+      (base, update) => {
+        merge(base, update);
+      }
+    );
+  }
+
+  return baseEntryClone;
+};
+
+// ------------------------------------
+
+function updateArrayField<TField>(
+  baseValues: TField[],
+  updateValues: TField[],
+  matcher: (base: TField, update: TField) => boolean,
+  updater: (base: TField, update: TField) => void
+): void {
+  updateValues.forEach(update => {
+    const base = baseValues.find(base => matcher(base, update));
+
+    if (base) {
+      updater(base, update);
+    } else {
+      baseValues.push(update);
+    }
+  });
+}
