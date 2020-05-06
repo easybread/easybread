e192491dea3a1e04132833df36d3bd66f469beca
Aleksander Cherednichenko <alexandr2110pro@gmail.com>
2020-05-06 13:50:12 +0300

    fix(operations): correct EmployeeByIdOperation name

--- packages/operations/src/employee/employee-by-id.operation.ts
+++ packages/operations/src/employee/employee-by-id.operation.ts
@@ -8,15 +8,15 @@ import { BreadSchema, PersonSchema } from '@easybread/schemas';
 import { BreadOperationName } from '../bread.operation-name';
 
 export interface EmployeeByIdOperation<TOutputRawData extends object = {}>
-  extends BreadOperation<BreadOperationName.EMPLOYEE_UPDATE> {
+  extends BreadOperation<BreadOperationName.EMPLOYEE_BY_ID> {
   input: BreadOperationInputWithParams<
-    BreadOperationName.EMPLOYEE_UPDATE,
+    BreadOperationName.EMPLOYEE_BY_ID,
     // TODO: come up with an interface for that
     Pick<BreadSchema, 'identifier'>
   >;
 
   output: BreadOperationOutputWithRawDataAndPayload<
-    BreadOperationName.EMPLOYEE_UPDATE,
+    BreadOperationName.EMPLOYEE_BY_ID,
     TOutputRawData,
     PersonSchema
   >;
