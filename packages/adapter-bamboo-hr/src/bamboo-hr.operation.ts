9fb44b0cce0ca157259614d4042d9d29f7ef2add
Aleksander Cherednichenko <alexandr2110pro@gmail.com>
2020-05-06 13:52:16 +0300

    feat(adapter-bamboo-hr): Employee by id operation handler

--- packages/adapter-bamboo-hr/src/bamboo-hr.operation.ts
+++ packages/adapter-bamboo-hr/src/bamboo-hr.operation.ts
@@ -1,14 +1,20 @@
 import {
+  EmployeeByIdOperation,
   EmployeeCreateOperation,
   EmployeeSearchOperation,
   EmployeeUpdateOperation,
   SetupBasicAuthOperation
 } from '@easybread/operations';
 
-import { BambooBasicAuthPayload, BambooEmployeesDirectory } from './interfaces';
+import {
+  BambooBasicAuthPayload,
+  BambooEmployee,
+  BambooEmployeesDirectory
+} from './interfaces';
 
 export type BambooHrOperation =
   | SetupBasicAuthOperation<BambooBasicAuthPayload>
   | EmployeeSearchOperation<BambooEmployeesDirectory>
+  | EmployeeByIdOperation<BambooEmployee>
   | EmployeeCreateOperation
   | EmployeeUpdateOperation;
--- packages/adapter-bamboo-hr/src/handlers/index.ts
+++ packages/adapter-bamboo-hr/src/handlers/index.ts
@@ -1,4 +1,5 @@
 export { BambooSetupBasicAuthHandler } from './bamboo.setup-basic-auth.handler';
+export { BambooEmployeeByIdHandler } from './bamboo.employee-by-id.handler';
 export { BambooEmployeeSearchHandler } from './bamboo.employee-search.handler';
 export { BambooEmployeeCreateHandler } from './bamboo.employee-create.handler';
 export { BambooEmployeeUpdateHandler } from './bamboo.employee-update.handler';
