80aca43e62d253e315f155205651b06c86365be0
Aleksander Cherednichenko <alexandr2110pro@gmail.com>
2020-05-06 20:44:32 +0300

    fix(adapter-bamboo): query skypeUsername field in employee by id handler

--- packages/adapter-bamboo-hr/src/handlers/bamboo.employee-by-id.handler.ts
+++ packages/adapter-bamboo-hr/src/handlers/bamboo.employee-by-id.handler.ts
@@ -45,7 +45,8 @@ export const BambooEmployeeByIdHandler: BreadOperationHandler<
           'preferredName',
           'workEmail',
           'workPhone',
-          'workPhoneExtension'
+          'workPhoneExtension',
+          'skypeUsername'
         ]
       }
     });
