ba82c785227e1c9a05a8a0728d1d1bc40ecfca54
Aleksander Cherednichenko <alexandr2110pro@gmail.com>
2020-05-23 14:28:34 +0300

    feat(core): use pausable invocation in BreadOAuth2AuthStrategy.authorizeHttp()

--- packages/core/src/auth-strategy/bread.oauth2.auth-strategy.ts
+++ packages/core/src/auth-strategy/bread.oauth2.auth-strategy.ts
@@ -1,3 +1,4 @@
+import { PausableExecution } from '@easybread/common';
 import { AxiosRequestConfig } from 'axios';
 
 import { BreadAuthStrategy } from './bread.auth-strategy';
@@ -6,23 +7,34 @@ import { BreadOauth2StateData } from './interfaces';
 export abstract class BreadOAuth2AuthStrategy<
   TStateData extends BreadOauth2StateData
 > extends BreadAuthStrategy<TStateData> {
+  private authorizeHttpPausableExecution = new PausableExecution();
+
   async authorizeHttp(
     breadId: string,
     requestConfig: AxiosRequestConfig
   ): Promise<AxiosRequestConfig> {
     const authData = await this.readAuthData(breadId);
 
-    if (this.isExpired(authData)) {
-      // refresh
+    if (
+      this.isExpired(authData) &&
+      !this.authorizeHttpPausableExecution.isPaused
+    ) {
+      this.authorizeHttpPausableExecution.pause();
       await this.refreshToken(breadId);
+
+      this.authorizeHttpPausableExecution.resume();
       // retry
       return this.authorizeHttp(breadId, requestConfig);
     }
 
-    return this.addAuthorizationHeader(
-      requestConfig,
-      `Bearer ${authData.accessToken}`
-    );
+    return this.authorizeHttpPausableExecution.add(async () => {
+      // TODO: optimize. We don't need to read it twice when the token is not expired.
+      const { accessToken } = await this.readAuthData(breadId);
+      return this.addAuthorizationHeader(
+        requestConfig,
+        `Bearer ${accessToken}`
+      );
+    });
   }
 
   protected isExpired(authData: BreadOauth2StateData): boolean {
@@ -32,7 +44,7 @@ export abstract class BreadOAuth2AuthStrategy<
   }
 
   protected createExpirationDate(expireTimeInMS: number): string {
-    // it's now in ms + expires_in in ms - 1min in ms
+    // it's "now" in ms + expires_in in ms - 1min in ms
     return new Date(Date.now() + expireTimeInMS - 60 * 1000).toISOString();
   }
 
