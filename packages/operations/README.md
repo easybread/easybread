1b6faf310f52557a5757913d52f5ad096a881258
Alexandr Cherednichenko <alexandr2110pro@gmail.com>
2025-04-15 20:35:11 +0400

    fix: rm unused @easybread/operations package

--- packages/operations/README.md
+++
@@ -1,3 +0,0 @@
-# EasyBREAD Operations
-
-> DEPRECATED
--- packages/operations/src/lib/job-applicant/job-applicant-search.operation.input-params.ts
+++
@@ -1,7 +0,0 @@
-import type { ApplyActionSchema } from '@easybread/schemas';
-
-export type JobApplicantSearchOperationInputParams = {
-  query?: string;
-  actionStatus?: ApplyActionSchema['actionStatus'];
-  startTime?: string;
-};
